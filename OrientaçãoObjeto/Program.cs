992db3ba0889278fe19b6fb388994bee5fc2ddad
Igor Augusto <igor-augustofc@hotmail.com>
2023-02-24 01:12:32 -0300

    First commit OO

---
+++ OrientaçãoObjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using OrientaçãoObjeto;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto("TV", 500.00, 10);
+
+            produto.Nome = "T";
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Preco);
+
+        }
+    }
+}
