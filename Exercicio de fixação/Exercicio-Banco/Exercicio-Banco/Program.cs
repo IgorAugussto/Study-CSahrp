7b453cc57d052eab5a077ecceb9d5813e4652fd6
Igor Augusto <igor-augustofc@hotmail.com>
2023-02-28 17:34:51 -0300

    Exercise

---
+++ Exercicio de fixação/Exercicio-Banco/Exercicio-Banco/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercicio_Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco conta;
+
+            //Lendo os dados iniciais
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Havera depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Entre com o deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new Banco(numero, titular, depositoInicial);
+            }
+            else
+            {
+                //Esse construtor vai jogar o saldo zero na minha conta
+                conta = new Banco(titular, numero);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine());
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            //"quantia" está sem a palavra double pois ela j´´a está declarada acima
+            quantia = double.Parse(Console.ReadLine());
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(conta);
+        }
+    }
+}
--- OrientaçãoObjeto/Produto.cs
+++ OrientaçãoObjeto/Produto.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 namespace OrientaçãoObjeto
 {
+    /*Ordem recomendada para implemntação dos membros
+     
+     * Atributos Privados
+     * Propriedades autoimplementadas
+     * Construtores
+     * Propriedades Customizadas
+     * Outros métodos da classe
+     
+     Essa ordem está sendo seguida nessa classe Produto*/
     internal class Produto
     {
+        //Como o nome tem uma propriedade particular que no caso é a condição "if", vamos manter com o underline normal
         private string _nome;
 
         //Isso é o auto properties, uma maneira mais facil de declarar gets e sets
         public double Preco { get; private set; }
         public int Quantidade { get; private set; }
+
+        public Produto()
+        {
+
+        }
 
         public Produto(string nome, double preco, int quantidade)
         {
@@ -21,27 +36,38 @@ namespace OrientaçãoObjeto
             Quantidade = quantidade;
         }
 
+        public string Nome
+        {
+            get { return _nome; }
+            //O valor para atualizar o nome no metodo set é o value
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
         public double ValorEmEstoque()
         {
             return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
         }
 
         public override string ToString()
         {
             return "Nome: " + _nome + "\nValor: $" + Preco.ToString("F2") + "\nQuantidade: " + Quantidade 
                 + " unidades, \nTotal: $" + ValorEmEstoque().ToString("F2");
-        }
-
-        public string Nome
-        {
-            get { return _nome; }
-            //O valor para atualizar o nome no metodo set é o value
-            set {
-                if (value != null && value.Length > 1)
-                {
-                    _nome = value;
-                }
-            }
         }
     }
 }
